ad730d976d19943a4b0c362189c80f515fecfca6
Willy Scheibel <willyscheibel@gmx.de>
2013-10-20 21:36:11 +0200

    Add comment indicating a bug

--- source/main.cpp
+++ source/main.cpp
@@ -3,12 +3,9 @@
 #include <QWidget>
 
 #include "Application.h"
-
 #include "Viewer.h"
 #include "Canvas.h"
-
 #include "Painter.h"
-
 
 int main(int argc, char* argv[])
 {
@@ -16,41 +13,39 @@ int main(int argc, char* argv[])
 
     Application app(argc, argv);
 
-    {
-        QScopedPointer<Viewer> viewer(new Viewer());
+    QScopedPointer<Viewer> viewer(new Viewer());
 
-        QSurfaceFormat format;
+    QSurfaceFormat format;
 #ifdef NO_OPENGL_320
-        format.setVersion(3, 0);
+    format.setVersion(3, 0);
 #else
-        format.setVersion(3, 2);
-        format.setProfile(QSurfaceFormat::CoreProfile);
+    format.setVersion(3, 2);
+    format.setProfile(QSurfaceFormat::CoreProfile);
 #endif
-        Canvas * canvas = new Canvas(format);
-        canvas->setContinuousRepaint(true, 0);
-        canvas->setSwapInterval(Canvas::VerticalSyncronization);
+    Canvas* canvas = new Canvas(format);
+    canvas->setContinuousRepaint(true, 0);
+    canvas->setSwapInterval(Canvas::VerticalSyncronization);
 
-        QObject::connect(canvas, &Canvas::fpsUpdate, viewer.data(), &Viewer::fpsChanged);
-        QObject::connect(canvas, &Canvas::objUpdate, viewer.data(), &Viewer::objChanged);
-        QObject::connect(canvas, &Canvas::timeUpdate, viewer.data(), &Viewer::timeChanged);
-        QObject::connect(canvas, &Canvas::mouseUpdate, viewer.data(), &Viewer::mouseChanged);
-        QObject::connect(viewer.data(), &Viewer::toggleSwapInterval, canvas, &Canvas::toggleSwapInterval);
+    QObject::connect(canvas, &Canvas::fpsUpdate, viewer.data(), &Viewer::fpsChanged);
+    QObject::connect(canvas, &Canvas::objUpdate, viewer.data(), &Viewer::objChanged);
+    QObject::connect(canvas, &Canvas::timeUpdate, viewer.data(), &Viewer::timeChanged);
+    QObject::connect(canvas, &Canvas::mouseUpdate, viewer.data(), &Viewer::mouseChanged);
+    QObject::connect(viewer.data(), &Viewer::toggleSwapInterval, canvas, &Canvas::toggleSwapInterval);
 
+    Painter painter;
+    canvas->assignPainter(&painter);
 
-        Painter painter;
-        canvas->assignPainter(&painter);
+    QWidget * widget = QWidget::createWindowContainer(canvas);
+    widget->setMinimumSize(1, 1);
+    widget->setAutoFillBackground(false); // Important for overdraw, not occluding the scene.
+    widget->setFocusPolicy(Qt::TabFocus);
 
-        QWidget * widget = QWidget::createWindowContainer(canvas);
-        widget->setMinimumSize(1, 1);
-        widget->setAutoFillBackground(false); // Important for overdraw, not occluding the scene.
-        widget->setFocusPolicy(Qt::TabFocus);
+    viewer->setCentralWidget(widget);
+    viewer->show();
 
-        viewer->setCentralWidget(widget);
-        viewer->show();
+    result = app.exec();
+    canvas->assignPainter(nullptr);
 
-        result = app.exec();
-        canvas->assignPainter(nullptr);
-
-    }
     return result;
+    // QWidget seems to take double ownership of canvas and frees it doubled
 }
