eb479e809cddfdb6dbba7c50cb154561e40c9319
Willy Scheibel <willyscheibel@gmx.de>
2014-11-07 16:03:38 +0100

    Merge source directory of second exercise

--- source/Painter.h
+++ source/Painter.h
@@ -3,14 +3,19 @@
 
 #include <QMatrix4x4>
 #include <QMap>
+#include <QVector3D>
 
 #include "AbstractPainter.h"
 
 class QOpenGLShader;
 class QOpenGLShaderProgram;
+class QKeyEvent;
 
 class Camera;
-class UnitCube;
+class Terrain;
+class ScreenAlignedQuad;
+class Icosahedron;
+
 
 class Painter : public AbstractPainter
 {
@@ -28,23 +33,78 @@ public:
     virtual void update();
     virtual void update(const QList<QOpenGLShaderProgram *> & programs);
 
+    void keyPressEvent(QKeyEvent * event);
+
 protected:
     void paint_1_1(float timef);
+    void paint_1_2(float timef);
+    void paint_1_3(float timef);
+    void paint_1_4(float timef);
+    //void paint_1_5(float timef);
+    //...
+
+    void bindEnvMaps(GLenum target);
+    void unbindEnvMaps(GLenum target);
+
+    void paint_2_1_envmap(const int programIndex, float timef);
+    void paint_2_2_sphere(const int programIndex, float timef);
+
+    void paint_2_3_terrain(const int programIndex, float timef);
+    void paint_2_3_water(const int programIndex, float timef);
+
+    void paint_2_1(float timef);
+    void paint_2_2(float timef);
+    void paint_2_3(float timef);
+    void paint_2_4(float timef);
+    //void paint_2_5(float timef);
+    //...
+
+    enum EnvironmentMapping
+    {
+        CubeMapping
+    ,   PolarMapping
+    ,   ParaboloidMapping
+    ,   SphereMapping
+    };
 
 protected:
-    GLuint getOrCreateTexture(const QString & fileName);
-
     QOpenGLShaderProgram * createBasicShaderProgram(
         const QString & vertexShaderFileName
     ,   const QString & fragmentShaderFileName);
 
+    QOpenGLShaderProgram * createBasicShaderProgram(
+        const QString & vertexShaderFileName
+    ,   const QString & geometryShaderFileName
+    ,   const QString & fragmentShaderFileName);
+
 protected:
     Camera * m_camera;
+    
+    ScreenAlignedQuad * m_quad;
+    
+    EnvironmentMapping m_mapping;
+    QMap<EnvironmentMapping, GLuint> m_envmaps;
 
-    UnitCube * m_cube; // Note: This is for the test rendering
+    Icosahedron * m_icosa;
+    QVector3D m_icosa_center;
 
+    QList<Terrain *> m_terrains;
     QList<QMatrix4x4> m_transforms;
 
     QMap<int, QOpenGLShaderProgram *> m_programs;
     QList<QOpenGLShader *> m_shaders;
+
+    GLuint m_height;
+    GLuint m_ground;
+    GLuint m_caustics;
+
+    GLuint m_cubeFBO;
+    GLuint m_cubeTex;
+    GLuint m_cubeDepthRB;
+
+
+    // ############
+    // ToDo: Remove
+    GLuint m_waterheights;
+    GLuint m_waternormals;
 };
